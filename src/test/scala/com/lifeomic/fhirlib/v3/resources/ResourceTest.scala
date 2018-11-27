fa11dc31aa0d37eaed72803b36e9798d96a3562a
Taylor Steinberg <taylor.steinberg@lifeomic.com>
2018-11-27 09:39:03 -0500

    Adds methods to find  values from

--- src/test/scala/com/lifeomic/fhirlib/v3/resources/ResourceTest.scala
+++ src/test/scala/com/lifeomic/fhirlib/v3/resources/ResourceTest.scala
@@ -2,6 +2,7 @@ package com.lifeomic.fhirlib.v3.resources
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.lifeomic.fhirlib.v3.datatypes.Coding
 import org.junit.{Assert, Test}
 
 class ResourceTest {
@@ -9,6 +10,61 @@ class ResourceTest {
   val mapper: ObjectMapper =
     new ObjectMapper()
       .registerModule(DefaultScalaModule)
+
+  @Test
+  def findCodings(): Unit = {
+
+    val url = "http://hl7.org/fhir/us/core/StructureDefinition/us-core-race"
+    val system = "http://hl7.org/fhir/v3/Race"
+
+    val codes = Seq("1002-5")
+
+    val coding =
+      codes
+          .map(code => {
+            s"""
+               |{
+               |  "system": "$system",
+               |  "code": "$code"
+               |}
+           """.stripMargin
+          })
+          .mkString("[", ",", "]")
+
+    val json: String =
+      s"""
+         |{
+         |  "extension": [
+         |    {
+         |      "url": "$url",
+         |      "valueCodeableConcept": {
+         |        "coding": $coding
+         |      }
+         |    }
+         |  ]
+         |}
+      """.stripMargin
+
+    val resource = mapper.readValue(json, classOf[Resource])
+
+    val res = resource.findCodings(url, system)
+
+    if (res.isEmpty) {
+      Assert.fail()
+    }
+
+    res.foreach(res => {
+
+      if (res.isEmpty) {
+        Assert.fail()
+      }
+
+      val coding = res.head
+
+      Assert.assertEquals("1002-5", coding.code.get)
+      Assert.assertEquals("http://hl7.org/fhir/v3/Race", coding.system.get)
+    })
+  }
 
   @Test
   def findCodes(): Unit = {
